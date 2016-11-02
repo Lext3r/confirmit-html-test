f73b2dd18c3a4fc3b084030ec5677b0b3b327078
Lext3r <a7l8e9x5@gmail.com>
2016-11-02 12:43:54 +0300

    charts finally works

--- script.js
+++ script.js
@@ -2,19 +2,32 @@ window.onload = function(){
 
 	var select = document.getElementById('categories_select');
 	document.getElementById('cross').onclick = function() {
-		//select...
+		document.getElementById('categories_select').value = "none";
 	}
 
-	var f = function(){
+	var getData = function(){
 		let data = [];
 		let rows = document.getElementsByTagName('tr');
 		for (let i = 1; i < rows.length ; i++){
-			data[i] = rows[i].innerText.trim().split('\t');
-			let positive = (((data[i][3] / data[1][1]))*100).toFixed(2);
-			let negative = (Math.abs(data[i][2] / data[1][1])*100).toFixed(2);
-			rows[i].children[4].children[1].style.width = positive + "%";
-			rows[i].children[4].children[0].style.width = negative + "%";
+			data[i-1] = rows[i].innerText.trim().split('\t');
+		}
+		return data;
+	}
+
+	let data = getData();
+
+	drawChart = function(data){
+		let negative_width = (Math.abs(data[0][2] / data[0][1] * 100)).toFixed(2);
+		let positive_width = 99 - negative_width;
+		for(let i = 0; i < data.length; i++){
+			document.getElementsByClassName('chart_left')[i].style.width = negative_width + "%";
+			document.getElementsByClassName('chart_right')[i].style.width = positive_width + "%";
+			let negative = (Math.abs(data[i][2] / data[0][2] * 100)).toFixed(2);
+			let positive = (Math.abs(data[i][3] / data[0][3] * 100)).toFixed(2);
+			document.getElementsByClassName('negative')[i].style.width = negative + "%";
+			document.getElementsByClassName('positive')[i].style.width = positive + "%";
 		}
 	}
-	f();
+			
+	drawChart(data);
 }
