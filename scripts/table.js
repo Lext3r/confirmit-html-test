72386a285332c724a9de99bce8af0653bac01cec
Lext3r <a7l8e9x5@gmail.com>
2016-11-09 12:37:18 +0300

    minor fixes

--- scripts/table.js
+++ scripts/table.js
@@ -5,14 +5,26 @@ function createTable(id, rowNum, colNum){
             row.appendChild(document.createElement('th'));
         }
     for(var i = 1; i < rowNum; i++){
-        var row = table.insertRow(i);
+        var row = table.insertRow(i);    
         for(var j = 0; j < colNum; j++){
             row.insertCell(j);
         }
+        var chart = row.lastChild;
+        chart.id = 'table_chart';
+        addDiv(addDiv(chart, 'chart_left'),'negative');
+        addDiv(addDiv(chart, 'chart_right'),'positive');
     }
 }
 
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
+var max_negative, max_positive;
+
 function fillTable(id, keys, headers, num){
+    max_positive =  getMaxOfArray(getValuesByKey(data, 'positive', num - 1));
+    max_negative = getMaxOfArray(getValuesByKey(data, 'negative', num - 1));
     row = table.getElementsByTagName('tr')[0];
     for(j = 0; j < headers.length; j++){
             row.children[j].innerHTML = headers[j];
@@ -22,25 +34,23 @@ function fillTable(id, keys, headers, num){
         for(j = 0; j < keys.length; j++){
             row.children[j].innerHTML = data[i-1][keys[j]];
         }
-        var chart = row.lastChild;
-        chart.id = 'table_chart';
-        var left = addDiv(addDiv(left,'negative')t, 'chart_left');
-        var right = addDiv(addDiv(right,'positive'), 'chart_right');
         drawChart(i-1);
     }
 }
+
 function addDiv(parent, elClass){
         var div = document.createElement('div');
         div.setAttribute("class", elClass);
        return parent.appendChild(div);
 }
+
 function drawChart(i){
     var negative_width = (Math.abs(data[0]['negative'] / data[0]['total'] * 100)).toFixed(2);
-    var positive_width = 90 - negative_width;
+    var positive_width = 100 - negative_width;
     document.getElementsByClassName('chart_left')[i].style.width = negative_width + "%";
     document.getElementsByClassName('chart_right')[i].style.width = positive_width + "%";
-    var negative = (Math.abs(data[i]['negative'] / data[0]['negative'] * 100)).toFixed(2);
-    var positive = (Math.abs(data[i]['positive'] / data[0]['positive'] * 100)).toFixed(2);
+    var negative = (Math.abs(data[i]['negative'] / max_negative * 100)).toFixed(2);
+    var positive = (Math.abs(data[i]['positive'] / max_positive * 100)).toFixed(2);
     document.getElementsByClassName('negative')[i].style.width = negative + "%";
     document.getElementsByClassName('positive')[i].style.width = positive + "%";
 }
